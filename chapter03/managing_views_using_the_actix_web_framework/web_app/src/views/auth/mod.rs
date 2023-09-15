4661c2296a5845f60e6d7bbf9d36befe53a3012d
hurukan <42813681+truesoundlord@users.noreply.github.com>
2023-09-15 21:35:29 +0200

    fixing

--- chapter03/managing_views_using_the_actix_web_framework/web_app/src/views/auth/mod.rs
+++ chapter03/managing_views_using_the_actix_web_framework/web_app/src/views/auth/mod.rs
@@ -6,7 +6,7 @@ use actix_web::web::{ServiceConfig, get, scope};
 
 pub fn auth_views_factory(app: &mut ServiceConfig) {
     app.service(
-        scope("v1/auth")
+        scope("views/auth") // 'v1' doesn't exists in the filesystem  
             .route("login", get().to(login::login))
             .route("logout", get().to(logout::logout))
     );
